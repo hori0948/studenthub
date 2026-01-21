563cdb53fe1d796ec9a02fffd1ce810d725734fc
hori0948 <nrh0028@gmail.com>
2026-01-22 01:40:01 +0900

    Update index.blade.php
    検索機能とソート追加

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -7,39 +7,48 @@
 <head>
     <meta charset="UTF-8">
     <title>学生表示画面</title>
+
+    {{-- jQueryの読み込み --}}
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    {{-- 別ファイルにしたJSを読み込む --}}
+    <script src="{{ asset('js/student-search.js') }}"></script>
+
 </head>
 
 <body>
     <h1>学生表示</h1>
 
-    <table border="1">
-        <tr>
-            <th>学年</th>
-            <th>名前</th>
-            <th></th>
-        </tr>
+    {{-- 書き換え対象を id で囲む --}}
+    <div id="student-list">
+        <table border="1">
+            <tr>
+                <th><a href="#" id="sort-grade" data-order="asc">学年</a></th>
+                <th>名前</th>
+                <th></th>
+            </tr>
 
-        {{-- @forelse を使って「検索結果がない場合」を表示 --}}
-        @forelse ($students as $student)
-        <tr>
-            <td>{{ $student->grade }}年生</td>
-            <td>{{ $student->name }}</td>
-            <td>
-                <a href="{{  route('students.show', $student->id)}}" style="text-decoration: none;">
-                    <button type="button" style="cursor: pointer; padding: 5px 10px;">詳細</button>
-                </a>
-            </td>
-        </tr>
-        @empty
-        {{-- 検索結果が 0 件だった場合の表示 --}}
-        <tr>
-            <td colspan="3" style="text-align: center; color: red; padding: 10px;">
-                該当する学生はいませんでした。
-            </td>
-        </tr>
-        @endforelse
+            {{-- @forelse を使って「検索結果がない場合」を表示 --}}
+            @forelse ($students as $student)
+            <tr>
+                <td>{{ $student->grade }}年生</td>
+                <td>{{ $student->name }}</td>
+                <td>
+                    <a href="{{  route('students.show', $student->id)}}" style="text-decoration: none;">
+                        <button type="button" style="cursor: pointer; padding: 5px 10px;">詳細</button>
+                    </a>
+                </td>
+            </tr>
+            @empty
+            {{-- 検索結果が 0 件だった場合の表示 --}}
+            <tr>
+                <td colspan="3" style="text-align: center; color: red; padding: 10px;">
+                    該当する学生はいませんでした。
+                </td>
+            </tr>
+            @endforelse
 
-    </table>
+        </table>
+    </div>
 
     <br>
 
@@ -47,7 +56,7 @@
     <form action="{{ route('students.index') }}" method="GET">
         <div>
             <label>学年</label>
-            <select name="grade">
+            <select name="grade" id="search-grade"> {{-- id追加 --}}
                 <option value="">選択してください</option>
                 {{-- request('grade') の値に合わせて選択状態を保持 --}}
                 <option value="1" {{ request('grade') == '1' ? 'selected' : '' }}>1年生</option>
@@ -56,16 +65,14 @@
             </select>
         </div>
         <div>
-            <input type="text" name="studentname" placeholder="学生名" value="{{ request('studentname') }}">
-            <button type="submit">検索</button>
+            <input type="text" name="studentname" id="search-name" placeholder="学生名"> {{-- id追加 --}}
+            {{-- type="button" と id を追加 --}}
+            <button type="button" id="ajax-search-btn">検索</button>
 
-            {{-- ② 検索中（クエリパラメータがある時）だけ「全表示に戻る」ボタンを出す --}}
-            @if(request('grade') || request('studentname'))
-            <a href="{{ route('students.index') }}">
+            {{-- 全表示に戻るボタン：常にHTMLを出しておき、CSSで初期表示を制御 --}}
+            <a href="{{ route('students.index') }}" id="reset-button" style="display: none;">
                 <button type="button">全表示に戻る</button>
             </a>
-            @endif
-
         </div>
     </form>
 
